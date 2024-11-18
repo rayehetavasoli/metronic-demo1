3e3f00e862507ffd7cdaf37a43dd39c413e6f986
rayehe <rayehe.tavasoli@gmail.com>
2024-11-19 01:03:34 +0330

    fixing layouts for auth and main and import them in each pages

--- src/app/brands/page.tsx
+++ src/app/brands/page.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import {BrandTable} from '@/components/brandTable/index'
 import {brands} from '@/db/main'
+import MainLayout from '@/view/layout/MainLayout'
 
 function Brands() {
   return (
-    <BrandTable data={brands}/>
+    <MainLayout>
+      <BrandTable data={brands}/>
+    </MainLayout>
   )
 }
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import localFont from "next/font/local";
-import Header from '@/components/core/header'
-import Sidebar from '@/components/core/sidebar'
-import Footer from '@/components/core/footer'
-
 
 const mainFont = localFont({ src: "../assets/fonts/Vazir.ttf"});
 
@@ -21,18 +17,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fa">
-      <body
-        className={`${mainFont.style.fontFamily}`}
-      >
-         <div className="size-full justify-center items-start inline-flex min-h-lvh">
-      <div className="grow shrink basis-0  bg-light-bg-primary dark:bg-dark-bg-primary flex-col justify-between items-center inline-flex">
-        <Header/>
-        <div className="px-20 py-15 justify-center items-center my-16 size-full mx-10" >{children}</div>
-        <Footer/>
-      </div>
-      <Sidebar/>
-    </div>
-        
+      <body className={`${mainFont.style.fontFamily}`}>
+        {children}
       </body>
     </html>
   );
