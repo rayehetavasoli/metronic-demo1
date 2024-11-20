92c79af4bc87fd6f7f283634eee635a9925e5fe5
rayehe <rayehe.tavasoli@gmail.com>
2024-11-20 12:42:17 +0330

    add style for layout.tsx (size-full)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fa">
-      <body className={`${mainFont.style.fontFamily}`}>
+      <body className={`${mainFont.style.fontFamily}`+ 'size-full'}>
         {children}
       </body>
     </html>
